d6aed275ae97501b0f9aa15c80cf84be10d0fe6e
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-03-15 15:20:52 -0500

    ADD: neural_network.py > ANNESI > single_predict(): added single-predict helping-method.

--- src/neural_network.py
+++ src/neural_network.py
@@ -5,6 +5,7 @@ Author: Gijs G. Hendrickx
 """
 import logging
 
+import pandas as pd
 import torch
 
 from src import _backend
@@ -48,3 +49,48 @@ class ANNESI(_backend._NeuralNetwork):
         :rtype: list[str]
         """
         return check.input_check(*args)
+
+    def single_predict(
+            self, tidal_range, surge_level, river_discharge, channel_depth, channel_width, channel_friction,
+            convergence, flat_depth_ratio, flat_width, flat_friction, bottom_curvature, meander_amplitude,
+            meander_length
+    ):
+        """Predict output of a single set of input parameters. This single predictor provides more guidance in the type
+        of input; the `predict`- and `predict_from_file`-methods enable predictions based on multiple sets of input
+        parameters, i.e. based on an input data set.
+        :param tidal_range: tidal range
+        :param surge_level: storm surge level
+        :param river_discharge: river discharge
+        :param channel_depth: channel depth
+        :param channel_width: channel width
+        :param channel_friction: channel friction
+        :param convergence: convergence
+        :param flat_depth_ratio: flat depth ratio
+        :param flat_width: flat width
+        :param flat_friction: flat friction
+        :param bottom_curvature: bottom curvature
+        :param meander_amplitude: meander amplitude
+        :param meander_length: meander length
+        :type tidal_range: float
+        :type surge_level: float
+        :type river_discharge: float
+        :type channel_depth: float
+        :type channel_width: float
+        :type channel_friction: float
+        :type convergence: float
+        :type flat_depth_ratio: float
+        :type flat_width: float
+        :type flat_friction: float
+        :type bottom_curvature: float
+        :type meander_amplitude: float
+        :type meander_length: float
+        :return: neural network-based estimate of output
+        :rtype: pandas.DataFrame, float
+        """
+        # convert `dict` to `pandas.DataFrame`
+        data = pd.DataFrame({k: v for k, v in locals().items() if k in self.input_vars}, index=[0])
+
+        # predict
+        if len(self.output) == 1:
+            return float(self.predict(data, scan='full').values)
+        return self.predict(data, scan='full')
