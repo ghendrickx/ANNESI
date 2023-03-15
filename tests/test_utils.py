8bf211789c24e5b4981f214bb4a26b31ecc82b37
Gijs Hendrickx <ghendrickx@users.noreply.github.com>
2023-03-15 18:01:08 -0500

    DEL: test_utils.py > TestDefaultFileName: moved tests to `test_filing.py`.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,7 @@ import os
 
 import pytest
 
-from utils import filing, path
+from utils import path
 
 """pytest.fixtures"""
 
@@ -17,13 +17,6 @@ def save_init_dir_config():
     def dir_config(*home_dir):
         return path.DirConfig(*home_dir, create_dir=False)
     return dir_config
-
-
-@pytest.fixture
-def default_file_name_():
-    def auto_file_name(default, extension=None):
-        return filing._default_file_name(file_name='file', default=default, extension=extension)
-    return auto_file_name
 
 
 """TestClasses"""
@@ -132,39 +125,3 @@ class TestDirConfig:
     def test_config_dir_combined_self_str(self, save_init_dir_config):
         wd = save_init_dir_config().config_dir(save_init_dir_config('C:'), 'folder1', 'folder2')
         assert str(wd) == self.abs_dir
-
-
-class TestDefaultFileName:
-    """Tests for the _default_file_name-method."""
-
-    def test_default(self):
-        file_name = filing._default_file_name(file_name=None, default='default.txt')
-        assert file_name == 'default.txt'
-
-    def test_auto_ext(self, default_file_name_):
-        file_name = default_file_name_(default='default.txt')
-        assert file_name == 'file.txt'
-
-    def test_manual_ext(self, default_file_name_):
-        file_name = default_file_name_(default='default_ext.txt', extension='_ext.txt')
-        assert file_name == 'file_ext.txt'
-
-    def test_manual_ext_wrong_usage(self, default_file_name_):
-        file_name = default_file_name_(default='default_ext.txt')
-        assert file_name == 'file.txt'
-
-    def test_auto_ext_double(self):
-        file_name = filing._default_file_name(file_name='file.txt', default='default.txt')
-        assert file_name == 'file.txt'
-
-    def test_manual_ext_double(self):
-        file_name = filing._default_file_name(file_name='file_ext.txt', default='default_ext.txt', extension='_ext.txt')
-        assert file_name == 'file_ext.txt'
-
-    def test_auto_ext_xyz(self, default_file_name_):
-        file_name = default_file_name_(default='default.csv')
-        assert file_name == 'file.csv'
-
-    def test_auto_double_ext(self):
-        file_name = filing._default_file_name(file_name='file.txt', default='default.csv')
-        assert file_name == 'file.csv'
